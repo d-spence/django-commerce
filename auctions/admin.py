4eb65ffc239d8c9c0e650389c2c8bc4561be565d
Dan Spencer <dspence180@gmail.com>
2020-11-23 14:28:01 -0600

    Created database models. Index displays active listings now.

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Category, Auction, Comment, Bid
+
+
+class AuctionAdmin(admin.ModelAdmin):
+    """ customized admin options for Auction model """
+    list_display = ('title', 'id', 'user_id', 'category_id', 'date', 'active')
+    list_filter = ['date']
+    list_per_page = 20
+    search_fields = ['title']
+
+
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Auction, AuctionAdmin)
+admin.site.register(Comment)
+admin.site.register(Bid)
--- auctions/models.py
+++ auctions/models.py
@@ -3,4 +3,50 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    listings = models.ManyToManyField('Auction', blank=True, related_name="listed_by")
+    watched = models.ManyToManyField('Auction', blank=True, related_name="watched_by")
+
+    def __str__(self):
+        return f"{self.username} ({self.email})"
+
+
+# Additional models
+class Category(models.Model):
+    """ Category model; must be a unique name """
+    name = models.CharField(max_length=64, unique=True)
+    
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Auction(models.Model):
+    """ Primary data model for site auctions """
+    title = models.CharField(max_length=128)
+    active = models.BooleanField(default=True)
+    date = models.DateField(auto_now_add=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    current_bid = models.IntegerField()
+    image = models.ImageField(upload_to='auction_imgs')
+
+    def __str__(self):
+        return f"{self.title} ({self.category_id}) listed by {self.user_id}"
+
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=256)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing_id = models.ForeignKey(Auction, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.listing_id.title}: {self.user_id.username}: {self.comment}"
+
+
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing_id = models.ForeignKey(Auction, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.listing_id}: {self.user_id}: ${self.amount}"
